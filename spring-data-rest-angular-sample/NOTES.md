9effd7be0d0c763af9a18830ae1c593a1245aa6a
Matt Payne <MPayne@LinkedIn.com>
2016-04-09 05:55:20 -0500

    not working -- switching from mysql to h2 is wip

--- spring-data-rest-angular-sample/NOTES.md
+++ spring-data-rest-angular-sample/NOTES.md
@@ -1,3 +1,5 @@
 1. https://github.com/payne/SpringDataRestExample
 1. http://www.programming-free.com/2014/07/spring-data-rest-with-angularjs-crud.html
 
+1. https://spring.io/guides/gs/accessing-data-rest/
+
--- spring-data-rest-angular-sample/bin/data.sql
+++ spring-data-rest-angular-sample/bin/data.sql
@@ -1,5 +1,2 @@
 insert into task_list values(1,'Gathering Requirement','Requirement Gathering','MEDIUM','ACTIVE',curtime(),curtime() + INTERVAL 3 HOUR,0);
-insert into task_list values(2,'Application Designing','Application Designing','MEDIUM','ACTIVE',curtime(),curtime() + INTERVAL 2 HOUR,0);
-insert into task_list values(3,'Implementation','Implementation','MEDIUM','ACTIVE',curtime(),curtime() + INTERVAL 3 HOUR,0);
-insert into task_list values(4,'Unit Testing','Unit Testing','LOW','ACTIVE',curtime(),curtime() + INTERVAL 4 HOUR,0);
-insert into task_list values(5,'Maintanence','Maintanence','LOW','ACTIVE',curtime(),curtime() + INTERVAL 5 HOUR,0);
+
